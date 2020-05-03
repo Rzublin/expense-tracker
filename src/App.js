4a830e381d0cc4d0fdd09c4646047103d510e3b7
Raphael Zueblin <raphaelzublin@yahoo.com.br>
2020-05-03 12:34:56 +0200

    add header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
-
+import { Header } from "./components/Header";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
