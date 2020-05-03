378213f2462b6833a34748c6b8002112b6963aa5
Raphael Zueblin <raphaelzublin@yahoo.com.br>
2020-05-03 12:19:35 +0200

    Updates

---
+++ precache-manifest.87a9ef29b44bb43a6806cd5b1efcfe54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4211616a01ac67c6413e5a56eaffb20c",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "d3745692c3ef5580b70a",
+    "url": "/expense-tracker/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "45620eb22799239d819b",
+    "url": "/expense-tracker/static/js/2.458c31cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.458c31cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3745692c3ef5580b70a",
+    "url": "/expense-tracker/static/js/main.7206ae9d.chunk.js"
+  },
+  {
+    "revision": "479d3c9015d354e5eec8",
+    "url": "/expense-tracker/static/js/runtime-main.b761673c.js"
+  }
+]);
---
+++ static/js/main.7206ae9d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpexpense-tracker"]=this["webpackJsonpexpense-tracker"]||[]).push([[0],{3:function(e,t,n){e.exports=n(9)},8:function(e,t,n){},9:function(e,t,n){"use strict";n.r(t);var r=n(0),a=n.n(r),c=n(2),s=n.n(c);n(8);var o=function(){return a.a.createElement("div",{className:"App"})};s.a.render(a.a.createElement(a.a.StrictMode,null,a.a.createElement(o,null)),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.7206ae9d.chunk.js.map
