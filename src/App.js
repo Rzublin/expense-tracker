b7a667729c91bc953b27f30bcbbbd7e791e15ad0
Raphael Zueblin <raphaelzublin@yahoo.com.br>
2020-05-29 19:41:25 +0200

    add initial state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React from "react";
-import { Header } from "./components/Header";
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
+import TransactionList from "./components/TransactionList";
+import TransactionAddItem from "./components/TransactionAddItem";
 import "./App.css";
 
 function App() {
+  const [transactions, setTransactions] = useState([
+    { text: "Camera", amount: -200 },
+    { text: "Book", amount: -40 },
+    { text: "Cash", amount: 500 },
+  ]);
+  const removeTransaction = (transaction) => {
+    setTransactions(
+      transactions.filter((element) => element.text !== transaction.text)
+    );
+  };
   return (
-    <div className="App">
+    <div>
       <Header />
+      <div className="container">
+        <Balance transactions={transactions} />
+        <IncomeExpenses transactions={transactions} />
+        <TransactionList
+          transactions={transactions}
+          removeTransaction={removeTransaction}
+        />
+        <TransactionAddItem
+          transactions={transactions}
+          setTransactions={setTransactions}
+        />
+      </div>
     </div>
   );
 }
